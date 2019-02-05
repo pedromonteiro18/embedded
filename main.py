b35f9bda0967ed215c99ad80f598816ecea43d4b
pedromonteiro18 <pedroluizbrega@hotmail.com>
2019-02-05 11:31:39 +0000

    "Setting up MQTT"

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def on_message(client, userdata, message):
 
 client.on_message = on_message
 print("PrintTest")
-print("client.on_message is: " + client.on_message)
+print("client.on_message is: " + str(client.on_message))
 client.subscribe("IC.embedded/patriots/#")
 client.loop()
 
