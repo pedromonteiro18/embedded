008b31a4e65984ea97532fd5683ca9dc114bbbe8
pedromonteiro18 <pedroluizbrega@hotmail.com>
2019-02-07 12:16:47 +0000

    App

--- doorAlarm/App.js
+++ doorAlarm/App.js
@@ -23,7 +23,6 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>Test</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
